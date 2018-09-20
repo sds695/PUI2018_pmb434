5b240737939153d16255afacb14b775eaf0ae41f
Pablo Mandiola <pmandiolab@gmail.com>
2018-09-20 17:50:22 -0400

    handle lowercase bus line and errors

--- HW3_pmb434/show_bus_locations_pmb434.py
+++ HW3_pmb434/show_bus_locations_pmb434.py
@@ -15,11 +15,11 @@ if not len(sys.argv) == 3:
 
 #Set parameters
 key = sys.argv[1]
-LineRef = sys.argv[2]
+LineRef = sys.argv[2].upper()
 version = "2"
 
 #Set API endpoint
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key={0}&version={1}&LineRef={2}".format(key, version, LineRef)
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key={}&version={}&LineRef={}".format(key, version, LineRef)
 
 #Read data
 response = urllib.urlopen(url)
@@ -27,15 +27,18 @@ data = response.read().decode("utf-8")
 dataDict = json.loads(data)
 
 #Get buses array
-buses = dataDict["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"][0]["VehicleActivity"]
+try:
+	buses = dataDict["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"][0]["VehicleActivity"]
+except KeyError:
+	print("Invalid Bus Line: {}".format(LineRef))
+else:
+	#Print output
+	print ("Bus Line: {}".format(LineRef))
 
-#Print output
-print ("Bus Line: {0}".format(LineRef))
+	print ("Number of Active Buses: {}".format(len(buses)))
 
-print ("Number of Active Buses: {0}".format(len(buses)))
-
-for n in range(len(buses)):
-	print("Bus {0} is at latitude {1} and longitude {2}".format(
-		n+1,
-		buses[n]["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"],
-		buses[n]["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"]))
+	for n in range(len(buses)):
+		print("Bus {} is at latitude {} and longitude {}".format(
+			n+1,
+			buses[n]["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"],
+			buses[n]["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"]))
